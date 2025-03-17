6f9e4ccbbe2dce1bc2616061852fe45c4a2376d7
lostluki1343 <lukas36@gmx.at>
2025-03-17 15:53:35 +0100

    first commit

---
+++ src/main/java/at/ac/htlstp/et/k5b/webservercsvauswertung/WebserverCsvAuswertungApplication.java
@@ -0,0 +1,13 @@
+package at.ac.htlstp.et.k5b.webservercsvauswertung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserverCsvAuswertungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebserverCsvAuswertungApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/ac/htlstp/et/k5b/webservercsvauswertung/WebserverCsvAuswertungApplicationTests.java
@@ -0,0 +1,13 @@
+package at.ac.htlstp.et.k5b.webservercsvauswertung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserverCsvAuswertungApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
